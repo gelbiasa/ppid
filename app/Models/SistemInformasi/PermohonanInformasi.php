c045c5aa3a511775fbf5ed6b04fa69c8625f456c
gelbiasa <gelbifirmansyah12@gmail.com>
2025-02-15 20:32:43 +0700

    push

---
+++ app/Models/SistemInformasi/PermohonanInformasi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\SistemInformasi;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PermohonanInformasi extends Model
+{
+    use HasFactory;
+}
