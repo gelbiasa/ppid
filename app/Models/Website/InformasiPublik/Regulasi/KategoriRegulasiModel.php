068d86568e1c3bfead1406a3cc63b87b6b1cb25b
Solikhin <solikhinkhin645@gmail.com>
2025-02-27 19:09:32 +0700

    push

--- app/Models/Website/InformasiPublik/Regulasi/KategoriRegulasiModel.php
+++ app/Models/Website/InformasiPublik/Regulasi/KategoriRegulasiModel.php
@@ -2,10 +2,55 @@
 
 namespace App\Models\Website\InformasiPublik\Regulasi;
 
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class KategoriRegulasiModel extends Model
+
+class KategoriRegulasiModel extends BaseModel
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = ' t_kategori_regulasi';
+    protected $primaryKey = 'kategori_reg_id';
+    protected $fillable = [
+        'fk_regulasi_dinamis',
+        'kr_kategori_reg_kode',
+        'kr_nama_kategori'
+    ];
+     public function RegulasiDinamis()
+    {
+        return $this->belongsTo(RegulasiDinamisModel::class, 'fk_regulasi_dinamis', 'regulasi_dinamis_id');
+    }
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->fillable = array_merge($this->fillable, $this->getCommonFields());
+    }
+
+    public static function selectData()
+    {
+      //
+    }
+
+    public static function createData()
+    {
+      //
+    }
+
+    public static function updateData()
+    {
+        //
+    }
+
+    public static function deleteData()
+    {
+        //
+    }
+
+    public static function validasiData()
+    {
+        //
+    }
 }
--- app/Models/Website/InformasiPublik/Regulasi/RegulasiModel.php
+++ app/Models/Website/InformasiPublik/Regulasi/RegulasiModel.php
@@ -2,10 +2,55 @@
 
 namespace App\Models\Website\InformasiPublik\Regulasi;
 
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class RegulasiModel extends Model
+class RegulasiModel extends BaseModel
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 't_regulasi';
+    protected $primaryKey = 'regulasi_id';
+    protected $fillable = [
+        'fk_m_kategori_regulasi',
+        'reg_judul',
+        'reg_sinopsis',
+        'reg_dokumen',
+        'reg_tipe_dokumen'
+    ];
+    public function KategoriRegulasi()
+    {
+        return $this->belongsTo(KategoriRegulasiModel::class, 'fk_m_kategori_regulasi',  'kategori_reg_id');
+    }
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->fillable = array_merge($this->fillable, $this->getCommonFields());
+    }
+
+    public static function selectData()
+    {
+      //
+    }
+
+    public static function createData()
+    {
+      //
+    }
+
+    public static function updateData()
+    {
+        //
+    }
+
+    public static function deleteData()
+    {
+        //
+    }
+
+    public static function validasiData()
+    {
+        //
+    }
 }
