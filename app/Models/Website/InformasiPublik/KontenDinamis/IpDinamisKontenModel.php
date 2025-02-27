06f620a3f7860f4481a86a12e47c0c094b0b08a3
Solikhin <solikhinkhin645@gmail.com>
2025-02-27 18:22:36 +0700

    push

--- app/Models/Website/InformasiPublik/KontenDinamis/IpDinamisKontenModel.php
+++ app/Models/Website/InformasiPublik/KontenDinamis/IpDinamisKontenModel.php
@@ -2,10 +2,48 @@
 
 namespace App\Models\Website\InformasiPublik\KontenDinamis;
 
+use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class IpDinamisKontenModel extends Model
+class IpDinamisKontenModel extends  BaseModel
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'm_ip_dinamis_konten';
+    protected $primaryKey = 'm_ip_konten_dinamis_id';
+    protected $fillable = [
+        'kd_nama_konten_dinamis'
+    ];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->fillable = array_merge($this->fillable, $this->getCommonFields());
+    }
+
+    public static function selectData()
+    {
+      //
+    }
+
+    public static function createData()
+    {
+      //
+    }
+
+    public static function updateData()
+    {
+        //
+    }
+
+    public static function deleteData()
+    {
+        //
+    }
+
+    public static function validasiData()
+    {
+        //
+    }
 }
